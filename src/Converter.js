ffae6dca663d624a2a62d4169817a92f26ae5dea
ali sait bilgi <alisaitbilgi@gmail.com>
2017-03-15 09:08:19 +0200

    Add files via upload

---
+++ src/Converter.js
@@ -0,0 +1,31 @@
+export function uglifyNumber(number, digit = 1) {
+
+  if (!Number.isInteger(number)) {
+    throw new Error(number + " is not integer, Please enter an integer number value");
+  }
+  if (!Number.isInteger(digit)) {
+    throw new Error(digit + " is not integer, Please enter an integer digit value");
+  }
+
+  const isNegative = number < 0 ? "-" : "";
+  digit = digit < 0 ? 0 : digit;
+  number = Math.abs(number);
+
+  if (number === 0) {
+    return 0;
+  }
+
+  if (number < 1000) {
+    return number;
+  }
+
+  const abbrv = ["k", "m", "b", "t", "p", "e", "z", "y"];
+  const power = parseInt(Math.log10(number) / 3, 10);
+  
+  if ((number / Math.pow(1000, power)).toFixed(digit).slice(-1) === "0") {
+    return isNegative + (number / Math.pow(1000, power)).toFixed(digit - 1) + abbrv[power - 1];
+  }
+
+  return isNegative + (number / Math.pow(1000, power)).toFixed(digit) + abbrv[power - 1];
+
+}
